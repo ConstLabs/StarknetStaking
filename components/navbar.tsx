5567e5ac46dd32bcfa8892c1e9fe2cb8ca32d206
cryptonerdcn <cryptonerdcn@gmail.com>
2024-11-27 01:39:30 +0900

    Fix.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,7 @@ export function Navbar() {
     <nav className="bg-background border-b">
       <div className="container mx-auto">
         <div className="flex justify-between h-16 items-center">
-          <img src="https://docs.starknet.io/_/img/starknet-logo-dark.svg" alt="logo" className="h-10" />
+          <img src="/Constlab_logo.svg" alt="Constlabs Logo" className="h-10" />
           <div className="hidden sm:ml-6 sm:flex sm:items-center">
             <ConnectModel />
           </div>
